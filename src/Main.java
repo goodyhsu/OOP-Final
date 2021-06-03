97bbf4fef4835b88b21704c4816b7db7c05f2d9b
yui1910 <b07902033@ntu.edu.tw>
2021-06-03 21:35:03 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Player p1 = new Dog(100, new Point(0, 0));
+        Player p2 = new Dog(150, new Point(300, 300));
+
+        /world
+        //controller
+        //view
+        //game start
+        //view launch
+    }
+}
