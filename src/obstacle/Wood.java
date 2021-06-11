3bd3bbfd4331be20a526b4fc2735785877bf0331
goodyhsu <b07902043@ntu.edu.tw>
2021-06-11 14:05:11 +0800

    wood

--- src/obstacle/Wood.java
+++ src/obstacle/Wood.java
@@ -1,13 +1,34 @@
 package obstacle;
 
 import model.SpriteCoordinate;
+import model.SpriteShape;
 
+import java.awt.*;
 import java.io.File;
 
 public class Wood extends DestroyableObstacle{
-    public Wood(File file, SpriteCoordinate coordinate){ super(file, coordinate); }
+    private SpriteShape shape;
+    public Wood(File file, SpriteCoordinate coordinate){
+        super(file, coordinate);
+        this.shape = new SpriteShape(new Dimension(146, 176),
+                new Dimension(33, 38), new Dimension(66, 105));
+    }
+
 
     @Override
     public void update(){}
-    public
+
+    @Override
+    public Rectangle getRange(){ return new Rectangle(location, shape.size); }
+
+    @Override
+    public Dimension getBodyOffset() {
+        return shape.bodyOffset;
+    }
+
+    @Override
+    public Dimension getBodySize() {
+        return shape.bodySize;
+    }
+
 }
